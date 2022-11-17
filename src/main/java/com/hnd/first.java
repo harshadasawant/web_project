a32b48f82fb8d22649310e0fe276c48ab8b32cac
harshada <harshadasawant22@gmail.com>
2022-11-17 12:24:51 +0530

    added ppt

--- src/main/java/com/hnd/first.java
+++ src/main/java/com/hnd/first.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class first
  */
-//@WebServlet("/first")
+@WebServlet("/first")
 public class first extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
